776c164860eb26112acc7895315d46a041f57b89
LucaTaglialatela <luca.taglialatela@gmail.com>
2024-01-05 16:49:10 +0900

    init generic implementation of merge_tsv.py

--- src/merge_tsv.py
+++ src/merge_tsv.py
@@ -12,59 +12,61 @@ def hasLine(lines, count):
 
 parser = argparse.ArgumentParser(description='Script for merging .tsv files.')
 
-parser.add_argument('filename1')
-parser.add_argument('filename2')
-parser.add_argument('filename3')
+parser.add_argument('file1', type=str, nargs=1)
+parser.add_argument('other', type=str, nargs='+')
     
 args = parser.parse_args()
 
 # Convert input files from .csv -> .tsv if applicable
-files = [args.filename1, args.filename2, args.filename3]
+files = args.file1 + args.other
 for i, f in enumerate(files):
     if '.csv' in f:
         csv_to_tsv(f)
-        files[i] = files[i].replace(".csv", ".tsv")
-
-at_f = open(f'{files[0]}', "r")
-gt_f = open(f'{files[1]}', "r")
-ra_f = open(f'{files[2]}', "r")
+    files[i] = open(f'{f.replace(".csv", ".tsv")}', "r")
 
 # Keep track of files to close
-files = [at_f, gt_f, ra_f]
+# files = [files[0], files[1], files[2]]
 
 # Extract header row information
-at_h = at_f.readline().upper().strip()
-at_cols = at_h.split('\t')
-AT_TIME_IDX = at_cols.index('TIME')
-AT_LANE_IDX = at_cols.index('LANE')
-gt_h = gt_f.readline().upper().strip()
-gt_cols = gt_h.split('\t')
-GT_TIME_IDX = gt_cols.index('TIME')
-GT_LANE_IDX = gt_cols.index('LANE')
-ra_h = ra_f.readline().upper().strip()
-ra_cols = ra_h.split('\t')
-RA_TIME_IDX = ra_cols.index('TIME')
-RA_LANE_IDX = ra_cols.index('LANE')
+headers = [f.readline().upper().strip() for f in files]
+cols = [h.split('\t') for h in headers]
+time_idxs = [c.index('TIME') for c in cols]
+lane_idxs = [c.index('LANE') for c in cols]
+
+# at_h = files[0].readline().upper().strip()
+# at_cols = at_h.split('\t')
+# AT_TIME_IDX = at_cols.index('TIME')
+# AT_LANE_IDX = at_cols.index('LANE')
+
+# gt_h = files[1].readline().upper().strip()
+# gt_cols = gt_h.split('\t')
+# GT_TIME_IDX = gt_cols.index('TIME')
+# GT_LANE_IDX = gt_cols.index('LANE')
+
+# ra_h = files[2].readline().upper().strip()
+# ra_cols = ra_h.split('\t')
+# RA_TIME_IDX = ra_cols.index('TIME')
+# RA_LANE_IDX = ra_cols.index('LANE')
 
 # Extract data rows
-at = at_f.readlines()
-gt = gt_f.readlines()
-ra = ra_f.readlines()
+# f_data = [f.readlines() for f in files] TODO
+at = files[0].readlines()
+gt = files[1].readlines()
+ra = files[2].readlines()
 
 # EOF counters
-c_at, c_gt, c_ra = 0, 0, 0
+eof_c = [0] * len(files)
 
 # Output file
-filename_without_extension = args.filename1.split('.')[0]
+filename_without_extension = args.file1[0].split('.')[0]
 out_f = open(f"{filename_without_extension}_merged.tsv", "a")
-# out_f.write(f"{at_h}\t\t{gt_h}\t\t{ra_h}\n")
-out_f.write(f"{at_h}\t\t{ra_h}\n")
+out_h = '\t\t'.join(headers)
+out_f.write(f"{out_h}\n")
 files.append(out_f)
 
-at_hasLine = hasLine(at, c_at)
-# gt_hasLine = hasLine(gt, c_gt)
-gt_hasLine = False
-ra_hasLine = hasLine(ra, c_ra)
+at_hasLine = hasLine(at, eof_c[0])
+gt_hasLine = hasLine(gt, eof_c[1])
+ra_hasLine = hasLine(ra, eof_c[2])
 
 while (at_hasLine or gt_hasLine or ra_hasLine):
     res = ''
@@ -72,19 +74,25 @@ while (at_hasLine or gt_hasLine or ra_hasLine):
 
     # Extract timestamp and lane number of current row
     if at_hasLine:
-        x_cols = re.split(r'\t+', at[c_at])
+        AT_TIME_IDX = time_idxs[0]
+        AT_LANE_IDX = lane_idxs[0]
+        x_cols = re.split(r'\t+', at[eof_c[0]])
         x_time = x_cols[AT_TIME_IDX]            # HH:MM:SS
         x_time = int(x_time.replace(':', ''))   # HHMMSS
         x_lane = int(x_cols[AT_LANE_IDX])
 
     if gt_hasLine:
-        y_cols = re.split(r'\t+', gt[c_gt])
+        GT_TIME_IDX = time_idxs[1]
+        GT_LANE_IDX = lane_idxs[1]
+        y_cols = re.split(r'\t+', gt[eof_c[1]])
         y_time = y_cols[GT_TIME_IDX]            # HH:MM:SS
         y_time = int(y_time.replace(':', ''))   # HHMMSS
         y_lane = int(y_cols[GT_LANE_IDX])
 
     if ra_hasLine:
-        z_cols = re.split(r'\t+', ra[c_ra])
+        RA_TIME_IDX = time_idxs[2]
+        RA_LANE_IDX = lane_idxs[2]
+        z_cols = re.split(r'\t+', ra[eof_c[2]])
         z_time = z_cols[RA_TIME_IDX]            # yyyy-mm-ddTHH:MM:SS.f+11:00
         z_time = re.split('T|\.', z_time)[1]    # HH:MM:SS
         z_time = int(z_time.replace(':', ''))   # HHMMSS
@@ -93,7 +101,7 @@ while (at_hasLine or gt_hasLine or ra_hasLine):
     # Calculate minimum timestamp, lane number
     minTime, minLane = min(
         (x_time, x_lane),
-        # (y_time, y_lane),
+        (y_time, y_lane),
         (z_time, z_lane)
     )
     
@@ -101,29 +109,28 @@ while (at_hasLine or gt_hasLine or ra_hasLine):
     if (at_hasLine and
         x_time - minTime <= 1 and
         x_lane == minLane):
-        res += at[c_at].strip() + '\t\t'
-        c_at += 1
+        res += at[eof_c[0]].strip() + '\t\t'
+        eof_c[0] += 1
     else:
-        res += (len(at_cols) + 1) * '\t'
+        res += (len(cols[0]) + 1) * '\t'
     
-    # if (gt_hasLine and
-    #     y_time - minTime <= 1 and
-    #     y_lane == minLane):
-    #     res += gt[c_gt].strip() + '\t\t'
-    #     c_gt += 1
-    # else:
-    #     res += (len(gt_cols) + 1) * '\t'
+    if (gt_hasLine and
+        y_time - minTime <= 1 and
+        y_lane == minLane):
+        res += gt[eof_c[1]].strip() + '\t\t'
+        eof_c[1] += 1
+    else:
+        res += (len(cols[1]) + 1) * '\t'
     
     if (ra_hasLine and
         z_time - minTime <= 1 and
         z_lane == minLane):
-        res += ra[c_ra].strip()
-        c_ra += 1
+        res += ra[eof_c[2]].strip()
+        eof_c[2] += 1
 
-    at_hasLine = hasLine(at, c_at)
-    # gt_hasLine = hasLine(gt, c_gt)
-    gt_hasLine = False
-    ra_hasLine = hasLine(ra, c_ra)
+    at_hasLine = hasLine(at, eof_c[0])
+    gt_hasLine = hasLine(gt, eof_c[1])
+    ra_hasLine = hasLine(ra, eof_c[2])
 
     out_f.write(res + '\n')
 
